ebc5f824a728e1b76f47deb608dc978ea1035b32
Jonathan Souza <souzajonathan570@gmail.com>
2022-08-17 19:24:42 -0300

    feat: add shadow in page

--- src/template/UserList/styles.ts
+++ src/template/UserList/styles.ts
@@ -3,6 +3,7 @@ import styled, { css } from 'styled-components';
 export const PageContainer = styled.div`
   ${({ theme }) => css`
     background-color: ${theme.colors.white};
+    box-shadow: 0 0 3.2rem 0 rgb(136 152 170 / 15%);
     border: ${theme.border.radius};
     display: flex;
     flex-direction: column;
@@ -15,7 +16,6 @@ export const HeadContainer = styled.div`
   ${({ theme }) => css`
     border-top-left-radius: ${theme.border.radius};
     border-top-right-radius: ${theme.border.radius};
-    box-shadow: 0 0 2rem 0 rgb(136 152 170 / 15%);
     display: flex;
     gap: 3rem;
     padding: 2rem 2.4rem;
